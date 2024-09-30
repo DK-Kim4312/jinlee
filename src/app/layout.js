cb16fe07266e0907fa82047f216f2315950c04c2
DK Kim <dkim693@wisc.edu>
2024-10-01 01:44:41 +0900

    fix margins

--- src/app/layout.js
+++ src/app/layout.js
@@ -43,7 +43,6 @@ export default function RootLayout({ children }) {
             </ul>
           </nav>
           <div className="content-area">
-
             {children}
           </div>
         </div>
